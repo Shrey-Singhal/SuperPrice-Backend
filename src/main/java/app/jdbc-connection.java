6d7de0adc4aeca9b9779ab3ce8cd5cb941dc3f32
Daniel De Luca <daniel@deluca.net.au>
2023-09-01 16:55:48 +1000

    Change Log: - Moved pom.xml; - Added jdbc-connection.java file to help manage connection to db & running of queries etc.

---
+++ src/main/java/app/jdbc-connection.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/* Class for managing the JDBC Connection to a MySQL database */
+public class jdbcConnection {
+    private static final String DATABASE = ""; // TODO: Add database file name
+
+    /* Add methods for getting data for each function / web-page - should run queries etc. */
+    // TODO:
+
+}
